5f1e08853d60f620eb721b92742cb24604e974c4
joserochadev <joserochadev@gmail.com>
2025-11-04 14:40:49 -0300

    add initial monorepo structure

---
+++ configs/eslint-config/base.js
@@ -0,0 +1,14 @@
+/** @type { import('eslint').Linter.Config } */
+module.exports = {
+  extends: ['@rocketseat/eslint-config/react'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+      },
+    ],
+  },
+}
---
+++ configs/eslint-config/node.js
@@ -0,0 +1,14 @@
+/** @type { import('eslint').Linter.Config } */
+module.exports = {
+  extends: ['@rocketseat/eslint-config/node'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+      },
+    ],
+  },
+}
